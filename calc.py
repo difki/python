b10aa273e042f8d0113fb71b97cedc68116de718
Ilya Stoliar <ilyas@oath.com>
2019-04-20 12:29:35 +0300

    Unit tests

---
+++ calc.py
@@ -0,0 +1,18 @@
+
+def add(x,y):
+    return x+y
+
+
+def sub(x,y):
+    return x-y
+
+
+def mul(x,y):
+    return x*y
+
+
+def dev(x,y):
+    return x/y
+
+
+
---
+++ calc_test.py
@@ -0,0 +1,17 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calc
+
+
+class TestCalculator:
+
+    def test_subtraction(self):
+        assert 2 == calc.sub(4, 2)
+
+    def test_addition(self):
+        assert 4 == calc.add(2, 2)
+
+    def test_mul(self):
+        assert 6 == calc.mul(3, 2)
